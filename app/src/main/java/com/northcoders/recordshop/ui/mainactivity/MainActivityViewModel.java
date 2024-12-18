208cb680a1a674fb89316ca83952ae0e4ccbe696
Kye <NovusAa12@proton.me>
2024-12-18 11:43:55 +0000

    feat: add MainActivityViewModel

---
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivityViewModel.java
@@ -0,0 +1,25 @@
+package com.northcoders.recordshop.ui.mainactivity;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.northcoders.recordshop.model.Album;
+import com.northcoders.recordshop.repository.AlbumRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    private AlbumRepository albumRepository;
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+
+        this.albumRepository = new AlbumRepository(application);
+    }
+
+    public LiveData<List<Album>> getAllAlbums() {
+        return albumRepository.getMutableLiveData();
+    }
+}
