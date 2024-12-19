eea1c5b3db4d3a39fce101f61d19c6864650bf23
Kye <NovusAa12@proton.me>
2024-12-19 16:13:26 +0000

    feat: add MainActivityClickHandler to MainActivity

--- app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivity.java
@@ -2,11 +2,7 @@ package com.northcoders.recordshop.ui.mainactivity;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -26,22 +22,18 @@ public class MainActivity extends AppCompatActivity {
     private List<Album> albumsFromViewModel = new ArrayList<>();
     private MainActivityViewModel mainActivityViewModel;
     private ActivityMainBinding binding;
+    private MainActivityClickHandler clickHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
-
+        clickHandler = new MainActivityClickHandler(this);
+        binding.setClickHandler(clickHandler);
 
         fetchAlbums();
     }
