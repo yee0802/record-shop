7ebf9f0a8fe87e34afa7dd1c928d75664884cf7a
Kye <NovusAa12@proton.me>
2024-12-19 11:07:44 +0000

    feat: add AlbumAdapter

---
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/AlbumAdapter.java
@@ -0,0 +1,58 @@
+package com.northcoders.recordshop.ui.mainactivity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.northcoders.recordshop.R;
+import com.northcoders.recordshop.databinding.AlbumItemLayoutBinding;
+import com.northcoders.recordshop.model.Album;
+
+import java.util.List;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder> {
+    private List<Album> albumList;
+    private Context context;
+
+    public AlbumAdapter(List<Album> albumList, Context context) {
+        this.albumList = albumList;
+        this.context = context;
+    }
+
+    public static class AlbumViewHolder extends RecyclerView.ViewHolder {
+        private AlbumItemLayoutBinding binding;
+
+        public AlbumViewHolder(@NonNull AlbumItemLayoutBinding albumItemLayoutBinding) {
+            super(albumItemLayoutBinding.getRoot());
+            this.binding = albumItemLayoutBinding;
+        }
+    }
+
+    @NonNull
+    @Override
+    public AlbumViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        AlbumItemLayoutBinding binding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.album_item_layout,
+                parent,
+                false
+        );
+
+        return new AlbumViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
+        Album album = albumList.get(position);
+        holder.binding.setAlbum(album);
+    }
+
+    @Override
+    public int getItemCount() {
+        return albumList.size();
+    }
+}
