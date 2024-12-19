3964ff3542404e815fb73c43f2f6ea21b2c48fa7
Kye <NovusAa12@proton.me>
2024-12-19 14:15:09 +0000

    feat: add addAlbum to AlbumRepository

--- app/src/main/java/com/northcoders/recordshop/repository/AlbumRepository.java
+++ app/src/main/java/com/northcoders/recordshop/repository/AlbumRepository.java
@@ -2,6 +2,7 @@ package com.northcoders.recordshop.repository;
 
 import android.app.Application;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.lifecycle.MutableLiveData;
 
@@ -16,16 +17,17 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class AlbumRepository {
+    private final Application application;
     private MutableLiveData<List<Album>> mutableLiveData;
+    private AlbumApiService albumApiService;
 
     public AlbumRepository(Application application) {
+        this.application = application;
         mutableLiveData = new MutableLiveData<>();
+        albumApiService = RetrofitInstance.getService();
     }
 
     public MutableLiveData<List<Album>> getMutableLiveData() {
-        AlbumApiService albumApiService = RetrofitInstance.getService();
-
-        assert albumApiService != null;
         Call<List<Album>> call = albumApiService.getAllAlbums();
 
         call.enqueue(new Callback<>() {
@@ -42,5 +44,22 @@ public class AlbumRepository {
         });
 
         return mutableLiveData;
+    }
+
+    public void addAlbum(Album album) {
+        Call<Album> call = albumApiService.addAlbum(album);
+
+        call.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<Album> call, Response<Album> response) {
+                Toast.makeText(application.getApplicationContext(), "Album added successfully", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Album> call, Throwable t) {
+                Toast.makeText(application.getApplicationContext(), "Unable to add Album", Toast.LENGTH_SHORT).show();
+                Log.e("AlbumRepository", "Error adding an album", t);
+            }
+        });
     }
 }
