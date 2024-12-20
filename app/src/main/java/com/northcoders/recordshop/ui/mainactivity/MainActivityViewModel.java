51e4ee2033e9b0fcbd8b8c7e1dd67145859ecfd9
Kye <NovusAa12@proton.me>
2024-12-20 11:54:27 +0000

    feat: add deleteAlbum to MainActivityViewModel

--- app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivityViewModel.java
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivityViewModel.java
@@ -30,4 +30,8 @@ public class MainActivityViewModel extends AndroidViewModel {
     public void updateAlbum(Long id, Album album) {
         albumRepository.updateAlbum(id, album);
     }
+
+    public void deleteAlbum(Long id) {
+        albumRepository.deleteAlbum(id);
+    }
 }
