4c7bc50d70a14dca739b66a5263743cd40e0ab47
Kye <NovusAa12@proton.me>
2024-12-20 11:41:01 +0000

    feat: add deleteAlbum to AlbumApiService

--- app/src/main/java/com/northcoders/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshop/service/AlbumApiService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
@@ -20,4 +21,7 @@ public interface AlbumApiService {
 
     @PUT("albums/{id}")
     Call<Album> updateAlbum(@Path("id") Long id, @Body Album album);
+
+    @DELETE("albums/{id}")
+    Call<String> deleteAlbum(@Path("id") Long id);
 }
