2ab5af2aafdcef667bf51a4de64dee5e9804f890
Kye <NovusAa12@proton.me>
2024-12-20 11:40:46 +0000

    feat: add updateAlbum to AlbumApiService

--- app/src/main/java/com/northcoders/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshop/service/AlbumApiService.java
@@ -8,10 +8,16 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface AlbumApiService {
     @GET("albums")
     Call<List<Album>> getAllAlbums();
 
     @POST("albums")
-    Call<Album> addAlbum(@Body Album album);}
+    Call<Album> addAlbum(@Body Album album);
+
+    @PUT("albums/{id}")
+    Call<Album> updateAlbum(@Path("id") Long id, @Body Album album);
+}
