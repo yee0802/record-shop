3621c9da63a22ddeb3be39eebbd74e43f235d742
Kye <NovusAa12@proton.me>
2025-01-07 14:22:11 +0000

    fix: remove deprecated binding from AddNewAlbumActivity

--- app/src/main/java/com/northcoders/recordshop/ui/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/northcoders/recordshop/ui/addalbum/AddNewAlbumActivity.java
@@ -7,25 +7,25 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.northcoders.recordshop.R;
-import com.northcoders.recordshop.databinding.ActivityAddNewAlbumBinding;
+import com.northcoders.recordshop.databinding.FragmentAddNewAlbumBinding;
 import com.northcoders.recordshop.model.Album;
 import com.northcoders.recordshop.model.Artist;
 import com.northcoders.recordshop.ui.mainactivity.MainActivityViewModel;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
-    ActivityAddNewAlbumBinding binding;
+    FragmentAddNewAlbumBinding binding;
     MainActivityViewModel viewModel;
     Album album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new_album);
+        setContentView(R.layout.fragment_add_new_album);
 
         album = new Album();
         album.setArtist(new Artist());
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_new_album);
+        binding = DataBindingUtil.setContentView(this, R.layout.fragment_add_new_album);
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
         binding.setAlbum(album);
